dfbcec259b1bf0fb65741ef90c171a5a9202a874
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2023-04-13 18:17:19 -0400

    Variable name refactor

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "cert_manager" {
   name             = "cert-manager"
-  namespace        = local.namespace
+  namespace        = var.cert_manager_namespace
   create_namespace = true
   repository       = "https://charts.jetstack.io"
   chart            = "cert-manager"
@@ -15,7 +15,7 @@ resource "helm_release" "cert_manager" {
 resource "kubernetes_secret_v1" "cloudflare_api_token" {
   metadata {
     name      = "cloudflare-api-token"
-    namespace = local.namespace
+    namespace = var.cert_manager_namespace
   }
 
   data = {
@@ -27,15 +27,20 @@ resource "kubernetes_secret_v1" "cloudflare_api_token" {
 
 resource "helm_release" "cert_manager_cluster_issuer" {
   depends_on = [helm_release.cert_manager]
-  namespace  = local.namespace
+  namespace  = var.cert_manager_namespace
   name       = "cert-manager-cluster-issuer"
   chart      = "${path.module}/files/cert-manager-cluster-issuer"
+  set {
+    name  = "acme.issuer"
+    value = var.cert_issuer_name
+  }
+
   set {
     name  = "cloudflare.apiToken.secretName"
     value = kubernetes_secret_v1.cloudflare_api_token.metadata.0.name
   }
   set {
     name  = "cloudflare.email"
-    value = var.cloudflare_api_email
+    value = var.cert_registration_email
   }
 }
