2d24582bd38ec8f2a7327308857a1a9ff860826e
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2023-04-13 18:16:45 -0400

    Add variables to control cert issuer configuration

--- variables.tf
+++ variables.tf
@@ -1,6 +1,18 @@
-variable "cloudflare_api_email" {
+variable "cert_manager_namespace" {
   type        = string
-  description = "Email address associated with the CloudFlare API token"
+  default     = "cert-manager"
+  description = "The namespace to deploy cert-manager"
+}
+
+variable "cert_issuer_name" {
+  type        = string
+  default     = "lets-encrypt"
+  description = "The certificate issuer name"
+}
+
+variable "cert_registration_email" {
+  type        = string
+  description = "Email address used for certificate registration"
 }
 
 variable "cloudflare_api_token" {
